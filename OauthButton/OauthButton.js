a92f0bdf2db4cf507188dbc44efae2edc8156534
Vladislav Baranchuk <vbaranchuk.ctr@virtru.com>
2019-07-08 17:34:45 +0300

    NOREF Add oauth button component (#16)

---
+++ OauthButton/OauthButton.js
@@ -0,0 +1,54 @@
+import React, { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+
+import GoogleIcon from './assets/google-icon.svg';
+import OutlookIcon from './assets/outlook-icon.svg';
+import Office365Icon from './assets/office365-icon.svg';
+import styles from './OauthButton.css';
+
+const VARIANT = {
+  GOOGLE: 'Google',
+  OUTLOOK: 'Outlook',
+  OFFICE365: 'Office365',
+};
+
+const ICONS = {
+  [VARIANT.GOOGLE]: <GoogleIcon />,
+  [VARIANT.OUTLOOK]: <OutlookIcon />,
+  [VARIANT.OFFICE365]: <Office365Icon />,
+};
+
+/**
+ * Renders oauth button with an icon
+ * @param children
+ * @param variant
+ * @param fullWidth
+ * @param props
+ * @param ref
+ * @returns {*}
+ * @constructor
+ */
+function OauthButton({ children, variant, fullWidth, ...props }, ref) {
+  const classNames = cn(styles.oauthButton, { [styles.fullWidth]: fullWidth });
+
+  return (
+    <button type="button" {...props} className={classNames} ref={ref}>
+      <span className={styles.icon}>{ICONS[variant]}</span>
+      <span>{children}</span>
+    </button>
+  );
+}
+
+OauthButton.propTypes = {
+  variant: PropTypes.oneOf(VARIANT).isRequired,
+  children: PropTypes.node,
+  fullWidth: PropTypes.bool,
+};
+
+OauthButton.defaultProps = {
+  children: null,
+  fullWidth: false,
+};
+
+export default Object.assign(forwardRef(OauthButton), { VARIANT });
