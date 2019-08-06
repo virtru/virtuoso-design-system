68c22ee88b24ef41a3eb0e87d80dfa35b61655ab
Shaw Yu <syu@virtru.com>
2019-08-06 11:35:29 -0500

    DASH-1288: Add Button component (#25)
    * add button component and simple story
    
    * small med large, primary secondary nooutline, hover focus active all handled
    
    * prettify / eslint
    
    * dump numerical for now
    
    * implement smallest/icon size and disabled states
    
    * add table for large buttons
    
    * fix svg alignment, icon size for text, nooutline disabled border color
    
    * size example svg to 16px
    
    * refactor table out into reusable function and reuse it a few times :)
    
    * prettier?
    
    * new baselines 👀
    
    * move variant/size class defs outside component
    
    * rename icon size to tiny
    
    * updated baselines
    
    * actually call it square
    
    * specificity?
    
    * jsdoc
    
    * stylelint
    
    * changelog and version to 0.4.0
    
    * changelog

---
+++ Button/Button.js
@@ -0,0 +1,69 @@
+import React, { forwardRef } from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+
+import styles from './Button.css';
+
+const VARIANT = {
+  PRIMARY: 'PRIMARY',
+  SECONDARY: 'SECONDARY',
+  NO_OUTLINE: 'NO_OUTLINE',
+};
+
+const SIZE = {
+  LARGE: 'LARGE',
+  MEDIUM: 'MEDIUM',
+  SMALL: 'SMALL',
+  SQUARE: 'SQUARE',
+};
+
+const variantClasses = {
+  [VARIANT.PRIMARY]: styles.primary,
+  [VARIANT.SECONDARY]: styles.secondary,
+  [VARIANT.NO_OUTLINE]: styles.noOutline,
+};
+const sizeClasses = {
+  [SIZE.LARGE]: styles.large,
+  [SIZE.MEDIUM]: styles.medium,
+  [SIZE.SMALL]: styles.small,
+  [SIZE.SQUARE]: styles.square,
+};
+
+/**
+ * Renders a button
+ * @param {Object} options
+ * @prop {String} variant
+ * @prop {String} size
+ * @prop {Boolean} fullWidth
+ * @param ref
+ * @returns {*}
+ * @constructor
+ */
+const Button = ({ variant, size, fullWidth, children, ...buttonProps }, ref) => {
+  const buttonClasses = cn(variantClasses[variant], sizeClasses[size], {
+    [styles.fullWidth]: fullWidth,
+  });
+
+  return (
+    // eslint-disable-next-line react/button-has-type
+    <button ref={ref} className={buttonClasses} {...buttonProps}>
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  variant: PropTypes.oneOf(Object.values(VARIANT)).isRequired,
+  size: PropTypes.oneOf(Object.values(SIZE)).isRequired,
+  type: PropTypes.oneOf(['button', 'submit', 'reset']),
+  fullWidth: PropTypes.bool,
+  children: PropTypes.node,
+};
+
+Button.defaultProps = {
+  type: 'button',
+  fullWidth: false,
+  children: null,
+};
+
+export default Object.assign(forwardRef(Button), { VARIANT, SIZE });
