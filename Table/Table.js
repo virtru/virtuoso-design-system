4067ca1e4200c65438636b5509d7b5a2d40e1dc8
Stephen Smith <ssmith@Stephen-Smiths-MacBook-Pro.local>
2019-08-09 13:53:46 -0400

    [DASH-1285] Added TBody element for optional use

--- Table/Table.js
+++ Table/Table.js
@@ -2,17 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import cn from 'classnames'
 
-import { TBL } from './table-selectors'
+import { TBL, TBL_BODY } from './table-selectors'
 
 import styles from './Table.css'
 
-const Table = ({ children, isScrollableBody }) => {
-  return (
-    <table className={cn(TBL, styles.table, { [isScrollableBody]: styles.scrollableBody })}>
-      {children}
-    </table>
-  )
-};
+const Table = ({ children, isScrollableBody }) => (
+  <table className={cn(TBL, styles.table, { [isScrollableBody]: styles.scrollableBody })}>
+    {children}
+  </table>
+);
 
 Table.propTypes = {
   children: PropTypes.node,
@@ -26,7 +24,10 @@ Table.defaultProps = {
 
 export default Table;
 
+const TBody = ({children}) => <tbody className={TBL_BODY}>{children}</tbody>
+
 export { default as THead } from './components/THead/THead';
+export { TBody }
 export { default as TH } from './components/TH/TH';
 
 export { default as TR } from './components/TR/TR';
