cccf94811850b247e457478665aa148b090eee41
Vladyslav Baranchuk <vbaranchuk.ctr@virtru.com>
2020-06-01 10:54:40 +0300

    CORE-714 Add ref for textarea (#71)

--- Textarea/Textarea.js
+++ Textarea/Textarea.js
@@ -1,47 +1,50 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 import ErrorIcon from './error.svg';
 import styles from './Textarea.css';
 
-const Textarea = ({ disabled, error, label, message, value, onChange, rows, ...textareaProps }) => {
-  const labelClass = classnames(styles.label, {
-    [styles.errorText]: !disabled && error,
-    [styles.disabled]: disabled,
-  });
+const Textarea = forwardRef(
+  ({ disabled, error, label, message, value, onChange, rows, ...textareaProps }, ref) => {
+    const labelClass = classnames(styles.label, {
+      [styles.errorText]: !disabled && error,
+      [styles.disabled]: disabled,
+    });
 
-  const textareaClass = classnames(styles.textarea, {
-    [styles.default]: !disabled && !error,
-    [styles.disabled]: disabled,
-    [styles.error]: !disabled && error,
-  });
+    const textareaClass = classnames(styles.textarea, {
+      [styles.default]: !disabled && !error,
+      [styles.disabled]: disabled,
+      [styles.error]: !disabled && error,
+    });
 
-  const hintMessageClass = classnames(styles.message, {
-    [styles.hint]: !disabled && !error,
-    [styles.disabled]: disabled,
-    [styles.errorText]: !disabled && error,
-  });
+    const hintMessageClass = classnames(styles.message, {
+      [styles.hint]: !disabled && !error,
+      [styles.disabled]: disabled,
+      [styles.errorText]: !disabled && error,
+    });
 
-  return (
-    <div className={styles.container}>
-      {label && <h2 className={labelClass}>{label}</h2>}
-      <textarea
-        className={textareaClass}
-        disabled={disabled}
-        value={value}
-        onChange={onChange}
-        rows={rows}
-        {...textareaProps}
-      />
-      {message && (
-        <span className={hintMessageClass}>
-          {!disabled && error && <ErrorIcon />} {message}
-        </span>
-      )}
-    </div>
-  );
-};
+    return (
+      <div className={styles.container}>
+        {label && <h2 className={labelClass}>{label}</h2>}
+        <textarea
+          ref={ref}
+          className={textareaClass}
+          disabled={disabled}
+          value={value}
+          onChange={onChange}
+          rows={rows}
+          {...textareaProps}
+        />
+        {message && (
+          <span className={hintMessageClass}>
+            {!disabled && error && <ErrorIcon />} {message}
+          </span>
+        )}
+      </div>
+    );
+  },
+);
 
 Textarea.propTypes = {
   disabled: PropTypes.bool,
