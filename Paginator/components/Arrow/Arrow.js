9389296991cec9f27c2efe3a550d8837f56e02d1
Vladyslav Baranchuk <vbaranchuk.ctr@virtru.com>
2020-06-23 10:42:27 +0300

    CORE-1234 Add paginator component (#81)

---
+++ Paginator/components/Arrow/Arrow.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './Arrow.css';
+
+const VARIANT = {
+  LEFT: 'left',
+  RIGHT: 'right',
+};
+
+const Arrow = ({ variant, ...props }) => (
+  <button type="button" className={styles[variant]} {...props} />
+);
+
+Arrow.propTypes = {
+  variant: PropTypes.oneOf(Object.values(VARIANT)).isRequired,
+};
+
+export default Object.assign(Arrow, { VARIANT });
