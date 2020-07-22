7555da1797971ad3eac8660c9320fb9490a92834
Ryan Schumacher <jschumacher@virtru.com>
2020-07-22 17:55:23 -0500

    Add virtru theme

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,10 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../stories/**/*.stories.js', '../stories/vrc-stories/*.js'],
   addons: [
     '@storybook/addon-actions', 
     '@storybook/addon-links',
-    '@storybook/addon-storysource',
     '@storybook/addon-knobs',
     '@storybook/addon-a11y',
     'storybook-addons-abstract/register',
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+import virtuosoTheme from './virtuosoTheme.js';
+
+addons.setConfig({
+  theme: virtuosoTheme,
+});
