ea95e6fa5ec486e272d58dca71481f23654248d0
Art Klepchukov <aklepchukov@virtru.com>
2020-07-22 14:49:10 -0700

    add storybook addon storybook-design-token
    with default design tokens, just to show the addon in action

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ['../stories/**/*.stories.js', '../stories/vrc-stories/*.js'],
   addons: [
+    'storybook-design-token',
     '@storybook/addon-actions', 
     '@storybook/addon-links',
     '@storybook/addon-storysource',
