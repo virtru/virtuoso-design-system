b549de616b0b88d495e509f1586ff754e0977352
Art Klepchukov <aklepchukov@virtru.com>
2020-07-22 21:24:45 -0700

    add custom Virtuoso light theme
    Despite the Storybook Theming docs, .storybook/manager.js was being ignored.

--- .storybook/manager.js
+++
@@ -1,6 +0,0 @@
-import { addons } from '@storybook/addons';
-import virtuosoTheme from './virtuosoTheme.js';
-
-addons.setConfig({
-  theme: virtuosoTheme,
-});
--- .storybook/virtuosoTheme.js
+++ .storybook/virtuosoTheme.js
@@ -1,37 +1,46 @@
 import { create } from '@storybook/theming/create';
 
 export default create({
-  // base: 'dark',
+  base: 'light',
 
-  // colorPrimary: 'hotpink',
+  // colorPrimary: '#3668FF',
   // colorSecondary: 'deepskyblue',
 
-  // // UI
-  // appBg: 'white',
-  // appContentBg: 'silver',
-  // appBorderColor: 'grey',
-  // appBorderRadius: 4,
+  // UI
+  appBg: '#DEEEFF',
+  appContentBg: '#FFF',
+  appBorderColor: '#C6CBD4',
+  appBorderRadius: 6,
 
   // // Typography
   // fontBase: '"Open Sans", sans-serif',
   // fontCode: 'monospace',
 
-  // // Text colors
-  // textColor: 'black',
+  // Text colors
+  textColor: '#2D323B',
   // textInverseColor: 'rgba(255,255,255,0.9)',
 
-  // // Toolbar default and active colors
-  // barTextColor: 'silver',
-  // barSelectedColor: 'black',
-  // barBg: 'hotpink',
+  // Toolbar default and active colors
+  barTextColor: '#777E8C',
+  barSelectedColor: '#001E4A',
+  barBg: '#F3F5F7',
 
-  // // Form colors
-  // inputBg: 'white',
-  // inputBorder: 'silver',
-  // inputTextColor: 'black',
-  // inputBorderRadius: 4,
+  // Form colors
+  inputBg: '#DEEEFF',
+  inputBorder: '#C6CBD4',
+  inputTextColor: '#2D323B',
+  inputBorderRadius: 3,
 
   brandTitle: 'Virtuoso Design System',
-  // brandUrl: 'https://example.com',
-  brandImage: 'https://inxmad4bw31barrx17wec71c-wpengine.netdna-ssl.com/wp-content/themes/salient-child/img/virtru-logo-white-rgb.svg',
+  brandUrl: 'https://www.virtru.com',
+  
+  // primary blue logo
+  // brandImage: 'https://inxmad4bw31barrx17wec71c-wpengine.netdna-ssl.com/wp-content/uploads/2019/11/virtru-logo-blue-250x93.png',
+  
+  // dark blue logo
+  brandImage: 'https://cdn-assets-cloud.frontify.com/local/frontify/eyJwYXRoIjoiXC9wdWJsaWNcL3VwbG9hZFwvc2NyZWVuc1wvMTAyODYzXC9mNjc4MDI2Njg1ZDhjM2UyMTk5NjA3MzU3YjY0MWRiOS0xNTc2MDg2MDEyLnBuZyJ9:frontify:n6xnf8Sq73Y0yXs09p2WRZHgmacZDGlYBdiuon39y6c?width=800',
+  
+  // black logo
+  // brandImage: 'https://cdn-assets-cloud.frontify.com/local/frontify/eyJwYXRoIjoiXC9wdWJsaWNcL3VwbG9hZFwvc2NyZWVuc1wvMTAyODYzXC8xZmUzZGE1ZDg4ZjkxMjJmMzcxZjUwNThjMDM3Y2UxNy0xNTc2MDg2MDQ3LnBuZyJ9:frontify:xEuTDou_Uq0TDP6Xb47GuWhIdB-wgccwHXm70P8o70g?width=800',
 });
+
