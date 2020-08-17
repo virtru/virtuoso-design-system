cb4552aa4f3d7aea95a6e042a41844fd5791a122
Art Klepchukov <aklepchukov@virtru.com>
2020-08-17 10:25:19 -0700

    docs(example app): show design tokens & fonts in action

--- examples/simple-app/index.js
+++ examples/simple-app/index.js
@@ -7,7 +7,55 @@ function SimpleApp() {
   return (
     <Layout>
       <Layout.Header title="My App"></Layout.Header>
-      Content goes here
+
+
+
+
+
+      <p>Example content showing design tokens in action:</p>
+
+
+
+
+
+      <ul>
+
+        <li className="ex__spacer--margin">
+          Am I at least a medium spacer apart from the next list item?
+        </li>
+        
+        <li className="ex__color--primaryBlue ex__spacer--padding">
+          This background should be primary blue with some ample padding.
+        </li>
+        
+        <li className="ex__spacer--padding ex__color--primarySlate ex__border--rounded">
+          I should be primary slate with a rounded border.
+        </li>
+
+        <li className="ex__shadow--hover">
+          Note my hovering shadow…
+        </li>
+
+      </ul>
+      
+      
+      
+      
+      
+      <section>
+
+        <h1 className="ex__head--xl">Heading XL</h1>
+        <h2 className="ex__head--lg">Heading LG</h2>
+        <h3 className="ex__head--md">Heading MD</h3>
+        <h4 className="ex__head--sm">Heading small</h4>
+        <h5 className="ex__head--xs">Heading xs</h5>
+        <h6 className="ex__head--xxs">Heading xs</h6>
+        
+        <p className="ex__body--md">Body copy</p>
+        <small className="ex__body--sm">Small text</small>
+
+      </section>
+
     </Layout>
   )
 }
