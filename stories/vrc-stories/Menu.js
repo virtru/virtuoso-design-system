ce496342c7be6ac3e7256e9814cdad9ef3eaa877
Ryan Schumacher <jschumacher@virtru.com>
2020-09-04 02:00:40 -0700

    feat(menu): enhance menu to add nav

--- stories/vrc-stories/Menu.js
+++ stories/vrc-stories/Menu.js
@@ -1,22 +1,52 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { boolean, select, text } from '@storybook/addon-knobs';
+import { number, select } from '@storybook/addon-knobs';
 
 import { Menu } from '@';
 
 const { MenuItem } = Menu;
 
-// eslint-disable-next-line react/prop-types
-const Container = ({ children }) => (
-  <div style={{ maxWidth: '450px', display: 'flex', flexGrow: '1' }}>{children}</div>
-);
+const component = ({ numberOfItems, ...props }) => {
+  let width;
+  let backgroundColor;
+  if (props.variant === Menu.VARIANTS.VARIANT_NAV) {
+    width = '100%';
+    backgroundColor = '#001E4A';
+  }
 
-storiesOf('Menu', module).lokiSkip('default', () => (
-  <Container>
-    <Menu>
-      <MenuItem>Hello</MenuItem>
-      <MenuItem>World</MenuItem>
-    </Menu>
-  </Container>
-))
+  return (
+    <div style={{ width, display: 'flex', flexGrow: '1', backgroundColor }}>
+      <Menu {...props}>
+        {[...Array(numberOfItems).keys()].map((n) => (
+          <MenuItem key={`menu-item-${n}`}>Menu Item {n}</MenuItem>
+        ))}
+      </Menu>
+    </div>
+  )
+};
+
+// storiesOf('Menu', module).lokiSkip('default', () => (
+//   <Container>
+//     <Menu>
+//       <MenuItem>Hello</MenuItem>
+//       <MenuItem>World</MenuItem>
+//     </Menu>
+//   </Container>
+// ));
+
+export default {
+  title: 'Menu',
+  parameters: {
+    loki: {
+      skip: true,
+    },
+  },
+};
+
+export const menu = () =>
+  component({
+    variant: select('variant', Menu.VARIANTS, Menu.VARIANTS_DEFAULT),
+    numberOfItems: number('number of items', 3, { min: 0 }),
+  });
+
+export const nav = () => component();
