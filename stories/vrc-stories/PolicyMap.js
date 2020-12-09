95e56dc5a4cb79295695fad89a83f36a39ec556f
Nathan Bakirci <nathan@virtru.com>
2020-12-09 15:28:18 -0500

    feat: add quartiles to policy map

--- stories/vrc-stories/PolicyMap.js
+++ stories/vrc-stories/PolicyMap.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { text, select } from '@storybook/addon-knobs';
+import { select } from '@storybook/addon-knobs';
 
 import { PolicyMap } from '@';
 import sampleGeoData from './assets/sampleGeoData.json';
@@ -10,6 +10,7 @@ const onCountryClick = (e) => {
   window.alert(JSON.stringify(e));
 };
 
-storiesOf('PolicyMap', module).lokiSkip('default', () => (
-  <PolicyMap data={sampleGeoData} onClick={onCountryClick} />
-));
+storiesOf('PolicyMap', module).lokiSkip('default', () => {
+  const focusQuartile = select('Focus quartile', [0, 1, 2, 3, 4]);
+  return <PolicyMap data={sampleGeoData} onClick={onCountryClick} focusQuartile={focusQuartile} />;
+});
