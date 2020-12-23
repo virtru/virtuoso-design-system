a7be24ccb07ecd986a7fd53970ed644e82922cd0
Nathan Bakirci <nathan@virtru.com>
2020-12-23 09:50:34 -0500

    feat: wip use blue color scale with clickable legend

--- stories/vrc-stories/PolicyMap.js
+++ stories/vrc-stories/PolicyMap.js
@@ -1,16 +1,22 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
-import { select } from '@storybook/addon-knobs';
 
 import { PolicyMap } from '@';
-import sampleGeoData from './assets/sampleGeoData.json';
+import sampleGeoData from './assets/sampleGeo2.json';
 
 const onCountryClick = (e) => {
   window.alert(JSON.stringify(e));
 };
 
 storiesOf('PolicyMap', module).lokiSkip('default', () => {
-  const focusQuartile = select('Focus quartile', [0, 1, 2, 3, 4]);
-  return <PolicyMap data={sampleGeoData} onClick={onCountryClick} focusQuartile={focusQuartile} />;
+  const [focus, setFocus] = useState(0);
+  return (
+    <PolicyMap
+      data={sampleGeoData}
+      onClick={onCountryClick}
+      focusQuartile={focus}
+      onFocusQuartile={setFocus}
+    />
+  );
 });
