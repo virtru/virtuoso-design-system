df25d04081878b122bd399b11cbe34c2289e85bb
Krish Suchak <suchak.krish@gmail.com>
2021-01-28 13:42:46 -0500

    feat(modal): title

--- stories/vrc-stories/ContentModal.js
+++ stories/vrc-stories/ContentModal.js
@@ -1,34 +1,48 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { text } from '@storybook/addon-knobs';
-import { Button, ContentModal } from '@';
+import { text, object } from '@storybook/addon-knobs';
+import { Button, ContentModal, Tabs, Tab, TabPanel, TabList } from '@';
 
+const tabLabels = ['Activity Feed', 'Access Control', 'Security Options'];
 const ModalContentLarge = () => (
-  <div
-    style={{
-      // background: styles.vds.color.white.value,
-      width: '1008',
-      // height: '812px',
-      height: '200px',
-    }}
-  />
+  // <div
+  //   style={{
+  //     // background: styles.vds.color.white.value,
+  //     width: '1008',
+  //     // height: '812px',
+  //     height: '200px',
+  //   }}
+  // />
+  <Tabs size={Tabs.SIZE.SMALL}>
+    <TabList>
+      {tabLabels.map((label) => (
+        <Tab>{label}</Tab>
+      ))}
+    </TabList>
+    {tabLabels.map((label) => (
+      <TabPanel>
+        <div style={{ padding: '20px' }}>Content for tab {label}</div>
+      </TabPanel>
+    ))}
+  </Tabs>
 );
 
 storiesOf('ContentModal', module).lokiSkip('default', () => {
-  const titleKey = text('Filename:');
-  const titleValue = text('january-reports.pdf');
-
+  const title = object('title', { key: 'Filename', value: 'january-reports.pdf' });
+  const subtitle = text(
+    'subtitle',
+    'Google Drive file shared by khart@acmecorp.com on November 16, 2020, 7:16 am',
+  );
   return (
     <ContentModal
       header={<Button variant={Button.VARIANT.SECONDARY} size={Button.SIZE.SQUARE} />}
-      titleKey={titleKey}
-      titleValue={titleValue}
+      title={title}
+      subtitle={subtitle}
       onRequestClose={() => alert('close')}
       onRequestBack={() => alert('back')}
     >
-      {' '}
-      {ModalContentLarge()}{' '}
+      {ModalContentLarge()}
     </ContentModal>
   );
 });
