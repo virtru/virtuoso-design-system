f13d0f965c8f727a757d34be2e4541c1418cb73d
Krish Suchak <suchak.krish@gmail.com>
2021-02-10 02:13:04 -0500

    feat: absolute position

--- stories/vrc-stories/ContentModal.js
+++ stories/vrc-stories/ContentModal.js
@@ -1,9 +1,41 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { text, object } from '@storybook/addon-knobs';
 import styles from 'lib/styles/build/js/design_tokens';
-import { Button, ContentModal, Tabs, Tab, TabPanel, TabList, Icon } from '@';
+import {
+  Button,
+  ContentModal,
+  Tabs,
+  Tab,
+  TabPanel,
+  TabList,
+  Icon,
+  Table,
+  TR,
+  TH,
+  TD,
+  THead,
+  TBody,
+} from '@';
+
+const getContentHeight = (varHeight = '0') => `calc( 100vh - 40px - 265px - ${varHeight}px)`;
+
+const noSubtitle = {
+  height: 'calc( 100vh - 40px - 265px)',
+};
+
+const contentStyle = {
+  overflowY: 'scroll',
+  // - overlay margins, rest of modal (base), tabs
+  height: 'calc( 100vh - 40px - 265px - 36px)',
+};
+
+const sample = [
+  { event: 'Activation Started', details: 'by user@domain.com', date: '2021-07-03' },
+  { event: 'Secure File Encrypted', details: 'by user@domain.com', date: '2020-05-02' },
+  { event: 'Secure Email Sent', details: 'by user@domain.com', date: '2019-07-01' },
+];
+const data = new Array(100).fill(sample).flat();
 
 const tabLabels = ['Activity Feed', 'Access Control', 'Security Options'];
 const ModalContentTabs = () => (
@@ -14,11 +46,36 @@ const ModalContentTabs = () => (
           <Tab>{label}</Tab>
         ))}
       </TabList>
-      {tabLabels.map((label) => (
+      {[
         <TabPanel>
-          <div style={{ padding: '20px' }}>Content for tab {label}</div>
-        </TabPanel>
-      ))}
+          <div style={contentStyle}>
+            <Table isScrollableBody>
+              <THead>
+                <TR>
+                  <TH>Event</TH>
+                  <TH>Details</TH>
+                  <TH>Date</TH>
+                </TR>
+              </THead>
+              <TBody>
+                {data.map((d) => (
+                  <TR>
+                    <TD>{d.event}</TD>
+                    <TD>{d.details}</TD>
+                    <TD>{d.date}</TD>
+                  </TR>
+                ))}
+              </TBody>
+            </Table>
+          </div>
+        </TabPanel>,
+        <TabPanel>
+          <div style={contentStyle} />
+        </TabPanel>,
+        <TabPanel>
+          <div style={contentStyle} />
+        </TabPanel>,
+      ]}
     </Tabs>
   </>
 );
@@ -28,7 +85,7 @@ const ModalContentPinkBox = () => (
     style={{
       background: styles.vds.color.red.lightest.value,
       width: '100%',
-      height: '150px',
+      height: 'calc( 100vh - 40px - 265px)',
     }}
   />
 );
