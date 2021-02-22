482dad099c0902d57292205b8b1ddf27f998f9f3
Krish Suchak <42231639+suchak1@users.noreply.github.com>
2021-02-22 10:46:46 -0500

    fix: add absolute positioning to content modal - CCR-17 (#124)
    * feat: change key to description
    
    * feat: reverse
    
    * feat: absolute position
    
    * chore: updated story
    
    * feat: content height func
    
    * chore: loki refs
    
    * feat: move modal down for page headers
    
    * fix: match mocks bottom overlay margin
    
    * fix: long title or subtitle
    
    * feat: add locators
    
    * fix: locators
    
    * fix: container overflow
    
    * feat: modal outer frame and header
    
    * feat: closer to completing refactor
    
    * feat: done
    
    * fix: padding
    
    * chore: loki refs
    
    * chore: loki refs
    
    * feat: finally done
    
    * feat: finally
    
    * fix(content modal): scrolling inside (#1)
    
    * chore: update story
    
    * chore: loki refs
    
    Co-authored-by: Vlad Baranchuk <kuguarpwnz@gmail.com>

--- stories/vrc-stories/ContentModal.js
+++ stories/vrc-stories/ContentModal.js
@@ -1,36 +1,61 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { text, object } from '@storybook/addon-knobs';
-import styles from 'lib/styles/build/js/design_tokens';
-import { Button, ContentModal, Tabs, Tab, TabPanel, TabList, Icon } from '@';
+import {
+  Button,
+  ContentModal,
+  Tabs,
+  Tab,
+  TabList,
+  TabPanel,
+  Icon,
+  Table,
+  TR,
+  TH,
+  TD,
+  THead,
+  TBody,
+} from '@';
 
-const tabLabels = ['Activity Feed', 'Access Control', 'Security Options'];
-const ModalContentTabs = () => (
-  <>
-    <Tabs size={Tabs.SIZE.SMALL}>
-      <TabList>
-        {tabLabels.map((label) => (
-          <Tab>{label}</Tab>
-        ))}
-      </TabList>
-      {tabLabels.map((label) => (
-        <TabPanel>
-          <div style={{ padding: '20px' }}>Content for tab {label}</div>
-        </TabPanel>
-      ))}
-    </Tabs>
-  </>
+const sample = [
+  { event: 'Activation Started', details: 'by user@domain.com', date: '2021-07-03' },
+  { event: 'Secure File Encrypted', details: 'by user@domain.com', date: '2020-05-02' },
+  { event: 'Secure Email Sent', details: 'by user@domain.com', date: '2019-07-01' },
+];
+const data = new Array(10).fill(sample).flat();
+
+const tabLabels = ['Activity Feed'];
+
+const TabLabels = () => (
+  <TabList>
+    {tabLabels.map((label) => (
+      <Tab>{label}</Tab>
+    ))}
+  </TabList>
 );
-
-const ModalContentPinkBox = () => (
-  <div
-    style={{
-      background: styles.vds.color.red.lightest.value,
-      width: '100%',
-      height: '150px',
-    }}
-  />
+const TabContentList = () => (
+  <TabPanel>
+    <div style={{ marginTop: '-1px' }}>
+      <Table isScrollableBody>
+        <THead>
+          <TR>
+            <TH>Event</TH>
+            <TH>Details</TH>
+            <TH>Date</TH>
+          </TR>
+        </THead>
+        <TBody>
+          {data.map((d) => (
+            <TR>
+              <TD>{d.event}</TD>
+              <TD>{d.details}</TD>
+              <TD>{d.date}</TD>
+            </TR>
+          ))}
+        </TBody>
+      </Table>
+    </div>
+  </TabPanel>
 );
 
 storiesOf('ContentModal', module)
@@ -45,20 +70,35 @@ storiesOf('ContentModal', module)
 
     return (
       <ContentModal
-        header={<Button variant={Button.VARIANT.SECONDARY} size={Button.SIZE.SQUARE} />}
+        modalHeader={<Button variant={Button.VARIANT.SECONDARY} size={Button.SIZE.SQUARE} />}
         title={title}
         subtitle={subtitle}
         titleIconName={titleIconName}
         subtitleIconName={subtitleIconName}
-        onRequestClose={() => alert('close')}
-        onRequestBack={() => alert('back')}
+        onCloseClick={() => alert('close')}
+        onBackClick={() => alert('back')}
+        contentHeader={TabLabels()}
+        render={(children) => (
+          <Tabs size={Tabs.SIZE.SMALL} scrollable>
+            {children}
+          </Tabs>
+        )}
       >
-        {ModalContentTabs()}
+        {TabContentList()}
       </ContentModal>
     );
   })
-  .add('no icons', () => (
-    <ContentModal title={{ value: 'Random Title' }} subtitle="Random subtitle">
-      {ModalContentPinkBox()}
+  .add('minimal', () => (
+    <ContentModal
+      title={{ value: 'Random Title' }}
+      subtitle="Random subtitle"
+      contentHeader={TabLabels()}
+      render={(children) => (
+        <Tabs size={Tabs.SIZE.SMALL} scrollable>
+          {children}
+        </Tabs>
+      )}
+    >
+      {TabContentList()}
     </ContentModal>
   ));
