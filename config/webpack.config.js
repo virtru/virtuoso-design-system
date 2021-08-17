bb8593f2fc1ded45884c7b1a27388e6643f4181b
sivanov <sivanov>
2021-08-17 14:26:03 +0300

    fix: im not sure what im doing anymore

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -110,22 +110,6 @@ module.exports = {
       },
     ],
   },
-  externals: {
-    react: {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react',
-      umd: 'react',
-    },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs2: 'react-dom',
-      commonjs: 'react-dom',
-      amd: 'react-dom',
-      umd: 'react-dom',
-    },
-  },
   plugins: [
     // new CleanWebpackPlugin(),
     // new CopyPlugin({
