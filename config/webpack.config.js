02ddde2a4a5ae067c84f19b2c1ca8367de9e0963
sivanov <sivanov>
2021-08-17 12:43:54 +0300

    fix: added cli

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -56,46 +56,46 @@ module.exports = {
       // },
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        // exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: [
               '@babel/preset-env',
-              // '@babel/preset-react',
+              '@babel/preset-react',
             ],
           },
         },
       },
-      {
-        test: /\.less$/i,
-        include: [path.join(__dirname, '..', 'node_modules', 'antd')],
-        loader: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'less-loader',
-            options: {
-              modifyVars: {
-                'primary-color': '#164EB6',
-                'primary-1': '#DEEEFF',
-                'primary-2': '#B2D6FF',
-                'primary-5': '#3668FF',
-                'primary-6': '#3668FF', // Core brand color
-                'primary-7': '#164EB6',
-                'success-color': '#75B749', // Primary green
-                'error-color': '#A70A0E', // Primary red
-                'btn-primary-bg': '#164EB6',
-                'disabled-bg': '#C6CBD4',
-                'btn-disable-border': '#C6CBD4',
-                'btn-disable-color': '#FFFFFF',
-                'btn-default-ghost-color': '#164EB6',
-              },
-              javascriptEnabled: true,
-            },
-          },
-        ],
-      },
+      // {
+      //   test: /\.less$/i,
+      //   include: [path.join(__dirname, '..', 'node_modules', 'antd')],
+      //   loader: [
+      //     'style-loader',
+      //     'css-loader',
+      //     {
+      //       loader: 'less-loader',
+      //       options: {
+      //         modifyVars: {
+      //           'primary-color': '#164EB6',
+      //           'primary-1': '#DEEEFF',
+      //           'primary-2': '#B2D6FF',
+      //           'primary-5': '#3668FF',
+      //           'primary-6': '#3668FF', // Core brand color
+      //           'primary-7': '#164EB6',
+      //           'success-color': '#75B749', // Primary green
+      //           'error-color': '#A70A0E', // Primary red
+      //           'btn-primary-bg': '#164EB6',
+      //           'disabled-bg': '#C6CBD4',
+      //           'btn-disable-border': '#C6CBD4',
+      //           'btn-disable-color': '#FFFFFF',
+      //           'btn-default-ghost-color': '#164EB6',
+      //         },
+      //         javascriptEnabled: true,
+      //       },
+      //     },
+      //   ],
+      // },
     ],
   },
   plugins: [
