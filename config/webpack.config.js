1d069d1775f5b8b8e133a08c8d2a54a8fb29fb4b
sivanov <sivanov>
2021-08-17 14:41:53 +0300

    fix: im not sure what im doing anymore

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
       // },
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
+        // exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
