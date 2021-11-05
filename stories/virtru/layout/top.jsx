dbb5c89cefe78d1096477f2b67a782b6ab27bfa6
sdubich <sdubich@virtru.com>
2021-11-05 12:24:10 +0200

    #CCR-479 added new layout

--- stories/virtru/layout/top.jsx
+++ stories/virtru/layout/top.jsx
@@ -1,35 +1,30 @@
 import 'antd/dist/antd.less';
 import { storiesOf } from '@storybook/react';
 import React from 'react';
-import { UserOutlined } from '@ant-design/icons';
-import { Layout, Header, Menu, TopMenuDropdown } from '@';
+import { Layout, Header, TopMenuDropdownWrap, Menu, TopMenuDropdown, CustomIconAvatar } from '@';
 
 const menu = (
-  <Menu defaultOpenKeys={['users']}>
-    <Menu.ItemGroup
-      key="users"
-      title="Signed-in Users"
-      style={{ fontSize: '1.3rem', color: 'black' }}
-    >
-      <Menu.Item key="1" icon={<UserOutlined />}>
+  <TopMenuDropdownWrap defaultSelectedKeys={['1']}>
+    <Menu.ItemGroup key="users" title="Signed-in Users">
+      <Menu.Item key="1" icon={<CustomIconAvatar />}>
         abc@virtru.com
       </Menu.Item>
-      <Menu.Item key="2" icon={<UserOutlined />}>
+      <Menu.Item key="2" icon={<CustomIconAvatar />}>
         xyz@virtru.com
       </Menu.Item>
     </Menu.ItemGroup>
     <Menu.Divider />
     <Menu.Item key="3">Sign in as different user</Menu.Item>
     <Menu.Item key="4">Sign out</Menu.Item>
-  </Menu>
+  </TopMenuDropdownWrap>
 );
 
 storiesOf('virtru/layout', module).add(
   'top',
   () => (
-    <Layout className="layout" style={{ width: "100%" }}>
-      <Header appName="Control Center">
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={[ "2" ]}>
+    <Layout className="layout" style={{ width: '100%' }}>
+      <Header appName="Control Center" homeUrl="/">
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
           <Menu.Item key="1">nav 1</Menu.Item>
           <Menu.Item key="2">nav 2</Menu.Item>
           <Menu.Item key="3">nav 3</Menu.Item>
@@ -38,7 +33,7 @@ storiesOf('virtru/layout', module).add(
       </Header>
 
       <br />
-      <Header appName="Control Center">
+      <Header appName="Control Center" homeUrl="/home">
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
           <Menu.Item key="1">nav 1</Menu.Item>
           <Menu.Item key="2">nav 2</Menu.Item>
@@ -57,16 +52,17 @@ storiesOf('virtru/layout', module).add(
       page: () => (
         <>
           <h1 id="enus">en-US</h1>
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
           <p>The most basic "header-content-footer" layout.</p>
           <p>
             Generally, the mainnav is placed at the top of the page, and includes the logo, the
             first level navigation, and the secondary menu (users, settings, notifications) from
             left to right in it. We always put contents in a fixed size navigation (eg:{' '}
-            <code>1200px</code>), the layout of the whole page is stable, it's not affected by
+            <code>1200px</code>), the layout of the whole page is stable, it`s not affected by
             viewing area.
           </p>
           <p>
-            Top-bottom structure is conform with the top-bottom viewing habit, it's a classical
+            Top-bottom structure is conform with the top-bottom viewing habit, it`s a classical
             navigation pattern of websites. This pattern demonstrates efficiency in the main
             workarea, while using some vertical space. And because the horizontal space of the
             navigation is limited, this pattern is not suitable for cases when the first level
