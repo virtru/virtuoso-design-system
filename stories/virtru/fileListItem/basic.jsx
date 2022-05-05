645f37944ce9d3b71911e520e94b52b3604e1430
mriabokon <mriabokon@lohika.com>
2022-05-05 16:17:34 +0300

    fix(docs/file-list-item): improved stories

---
+++ stories/virtru/fileListItem/basic.jsx
@@ -0,0 +1,46 @@
+import 'antd/dist/antd.less';
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { FileListItem, Row, Col, Button } from '@';
+
+import { iconTypeObject } from '@/components/icon/customIconTypeFile';
+
+const icons = Object.keys(iconTypeObject);
+
+const getRandomSize = () => (Math.random() * 900 + 100).toFixed(2);
+
+storiesOf('virtru/fileListItem', module).add(
+  'basic',
+  () => (
+    <Row gutter={[8, 8]}>
+      {icons.map((key) => (
+        <Col span={4} key={key}>
+          <FileListItem
+            name={key}
+            extension=".tdf"
+            decryptedExt={key}
+            size={`${getRandomSize()} KB`}
+            button={
+              <Button type="primary" size="small">
+                Remove
+              </Button>
+            }
+          />
+        </Col>
+      ))}
+    </Row>
+  ),
+  {
+    docs: {
+      page: () => (
+        <>
+          <h1 id="enus">en-US</h1>
+          <p>
+            There are <code>primary</code> button, <code>default</code> button, <code>dashed</code>{' '}
+            button, <code>text</code> button and <code>link</code> button in antd.
+          </p>
+        </>
+      ),
+    },
+  },
+);
---
+++ stories/virtru/fileListItem/withError.jsx
@@ -0,0 +1,39 @@
+import 'antd/dist/antd.less';
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { FileListItem, Button } from '@';
+
+const getRandomSize = () => (Math.random() * 900 + 100).toFixed(2);
+
+storiesOf('virtru/fileListItem', module).add(
+  'withError',
+  () => (
+    <div style={{ width: 250 }}>
+      <FileListItem
+        warning
+        name="file-with-error"
+        extension=".tdf"
+        decryptedExt="pdf"
+        size={`${getRandomSize()} KB`}
+        button={
+          <Button type="primary" size="small">
+            Remove
+          </Button>
+        }
+      />
+    </div>
+  ),
+  {
+    docs: {
+      page: () => (
+        <>
+          <h1 id="enus">en-US</h1>
+          <p>
+            There are <code>primary</code> button, <code>default</code> button, <code>dashed</code>{' '}
+            button, <code>text</code> button and <code>link</code> button in antd.
+          </p>
+        </>
+      ),
+    },
+  },
+);
