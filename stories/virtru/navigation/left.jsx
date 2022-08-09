0ee18cfd12e0feddd629ce4e899fa16308573a91
Sara Tarnvik <54858371+stvik@users.noreply.github.com>
2022-08-09 14:31:54 -0700

    feat(navigation): add lefthand navigation bar (#326)
    * feat(navigation): add lefthand navigation bar
    
    * fix(navigation): fixed lint issues
    
    * fix(navigation): fixed open/close logic and added tests

---
+++ stories/virtru/navigation/left.jsx
@@ -0,0 +1,32 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import { HomeOutlined, SettingFilled } from '@ant-design/icons';
+import { LeftNavigation, Menu } from '@';
+
+const menuItems = [
+  {
+    key: 'home',
+    icon: <HomeOutlined />,
+    label: 'Home',
+  },
+  {
+    key: 'settings',
+    icon: <SettingFilled />,
+    label: 'Settings',
+  },
+];
+
+storiesOf('virtru/navigation', module).add('left', () => {
+  return (
+    <LeftNavigation>
+      {menuItems.map((item) => {
+        return (
+          <Menu.Item key={item.key}>
+            {item.icon}
+            <span>{item.label}</span>
+          </Menu.Item>
+        );
+      })}
+    </LeftNavigation>
+  );
+});
