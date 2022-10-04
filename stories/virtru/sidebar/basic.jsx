49f1080803b61302bdfc540fe936a1facebb3a6d
gerus-virtru <102654459+gerus-virtru@users.noreply.github.com>
2022-10-04 18:44:24 +0200

    Update stories/virtru/sidebar/basic.jsx
    Co-authored-by: Nick <79929408+ntrevino-virtru@users.noreply.github.com>

--- stories/virtru/sidebar/basic.jsx
+++ stories/virtru/sidebar/basic.jsx
@@ -9,44 +9,34 @@ storiesOf('virtru/sidebar', module).add(
   'sidebar',
   () => {
     const [visibleDrawer, setVisibleDrawer] = useState(false);
-    const card1 = {
-      index: 1,
-      title: 'Session Information',
-      extra: (
-        <CloseOutlined
-          style={{ fontSize: '23px', color: 'var(--vds-color-slate-darker)', marginLeft: '10px' }}
-          onClick={() => {
-            console.log('click');
-            setVisibleDrawer(false);
-          }}
-        />
-      ),
-      cover: <div>Card body JSX</div>,
-    };
-    const card2 = {
-      index: 2,
-      title: 'Security',
-      extra: <Button danger>Revoke Access</Button>,
-    };
-
     return (
       <>
         <Button type="primary" onClick={() => setVisibleDrawer(!visibleDrawer)}>
           Open Drawer
         </Button>
         <Sidebar
-          {...{
-            visible: visibleDrawer,
-            children: (
-              <>
-                <SidebarCard {...card1} />
-                <SidebarCard {...card2} />
-              </>
-            ),
-            style: { marginTop: '1px' },
-            contentWrapperStyle: { width: '360px' },
-          }}
-        ></Sidebar>
+          visible={visibleDrawer}
+          style={{marginTop: '1px'}}
+          contentWrapperStyle={{ width: '360px' }}
+        >
+          <SidebarCard 
+            title="Session Information"
+            extra={(
+              <CloseOutlined
+                style={{ fontSize: '23px', color: 'var(--vds-color-slate-darker)', marginLeft: '10px' }}
+                onClick={() => {
+                  console.log('click');
+                  setVisibleDrawer(false);
+                }}
+              />
+            )}
+            cover={<div>Card body JSX</div>}
+          />
+          <SidebarCard 
+            title="Security"
+            extra={<Button danger>Revoke Access</Button>}
+          />
+        </Sidebar>
       </>
     );
   },
