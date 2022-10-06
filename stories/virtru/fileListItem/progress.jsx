6033832e4c0d142e5ecf854b28932eb25387919a
mriabokon <mriabokon@lohika.com>
2022-10-06 09:33:18 +0300

    refactor(storybook): moved controls to SB Knobs and joined stories

--- stories/virtru/fileListItem/progress.jsx
+++ stories/virtru/fileListItem/progress.jsx
@@ -1,62 +1,36 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, boolean, number } from '@storybook/addon-knobs';
 
-import { FileListItem, Button, Switch } from '@';
+import { FileListItem, Button } from '@';
 
-import 'antd/dist/antd.less';
+export default {
+  title: 'With progress',
+  decorators: [withKnobs],
+};
 
 storiesOf('virtru/fileListItem', module).add(
   'progress',
-  () => {
-    const [loading, setLoading] = useState(true);
-    const [progress, setProgress] = useState(30);
-
-    const checkboxChangeHandler = useCallback((checked) => {
-      setLoading(checked);
-    }, []);
-
-    const inputSlideChangeHandler = useCallback((e) => {
-      setProgress(+e.target.value);
-    }, []);
-
-    return (
-      <div style={{ width: 250 }}>
-        <Switch checked={loading} onChange={checkboxChangeHandler} /> Loading
-        <input
-          type="range"
-          min={0}
-          max={100}
-          value={progress}
-          style={{ width: '100%' }}
-          onChange={inputSlideChangeHandler}
-        />
-        <FileListItem
-          loading={loading}
-          progress={progress}
-          name="file-with-error"
-          extension=".tdf"
-          decryptedExt="pdf"
-          size="123.45 KB"
-          button={
-            <Button type="primary" size="small">
-              Remove
-            </Button>
-          }
-        />
-      </div>
-    );
-  },
+  () => (
+    <div style={{ width: 250 }}>
+      <FileListItem
+        loading={boolean('Loading', false)}
+        progress={number('Progress', 30, { range: true, min: 0, max: 100 })}
+        name="file-with-error"
+        extension=".tdf"
+        decryptedExt="pdf"
+        size="123.45 KB"
+        button={
+          <Button type="primary" size="small">
+            Remove
+          </Button>
+        }
+      />
+    </div>
+  ),
   {
     docs: {
-      page: () => (
-        <>
-          <h1 id="enus">en-US</h1>
-          <p>
-            There are <code>primary</code> button, <code>default</code> button, <code>dashed</code>{' '}
-            button, <code>text</code> button and <code>link</code> button in antd.
-          </p>
-        </>
-      ),
+      page: null,
     },
   },
 );
