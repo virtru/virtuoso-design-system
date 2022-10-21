addee9b748dafe1422ff9f8bc97fb20ad9353fc6
sdubich <sdubich@virtru.com>
2022-10-21 18:25:42 +0300

    #CCR-1493 fixed comments

--- stories/virtru/securitySettings/securitySettings.jsx
+++ stories/virtru/securitySettings/securitySettings.jsx
@@ -13,14 +13,6 @@ export default {
 storiesOf('virtru/securitySettings', module).add(
   'securitySettings',
   () => {
-    const showFilesSettings = true;
-    const securitySettings = {
-      expirationDateTime: null,
-      watermark: true,
-      persistentProtection: false,
-    };
-
-    const securitySettingsUpdating = false;
     const securitySettingChangeHandler = () => {
       // eslint-disable-next-line no-console
       console.log('securitySettingChangeHandler click');
@@ -30,11 +22,11 @@ storiesOf('virtru/securitySettings', module).add(
       <Row style={{ width: '100%' }}>
         <Col sspan={12} offset={6}>
           <SecuritySettingsToggles
-            showFilesSettings={showFilesSettings}
-            expirationDateTime={securitySettings.expirationDateTime}
-            watermark={boolean('Watermark', false)}
-            persistentProtection={boolean('PFP', true)}
-            disabled={securitySettingsUpdating}
+            showFilesSettings={boolean('Show file settings', true)}
+            expirationDateTime={null}
+            watermark
+            persistentProtection={false}
+            disabled={boolean('Disabled', false)}
             onChange={securitySettingChangeHandler}
           />
         </Col>
