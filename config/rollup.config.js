6db339d348cb70ce5dd0145224b8170880a183b0
Michael <100279167+riabokon-virtru@users.noreply.github.com>
2023-04-21 19:03:30 +0300

    INT-389: Added `editorStateToHTML` util function (#429)
    * fix(components/rich-text): fixed state updating
    
    * docs(storybook/rich-text): improved controlled story
    
    * feat(components/rich-text): dropped `value` and `defaultValue` support and replaced by `editorState`
    
    There is a bug in Draft-js when we try to control it by using a String value.
    
    BREAKING CHANGE: dropped `value` and `defaultValue` support
    
    * docs(storybook/rich-text): updated stories
    
    * test(vrt/rich-text): updated references
    
    * feat(utils/rich-text): added createEditorState function
    
    * feat(components/rich-text): implemented createEditorState util function
    
    * feat(build): improved Rollup config to provide utils
    
    * feat(utils/rich-text): added `editorStateToHTML` util function
    
    * feat(build): fixed Rollup config for utils

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,5 +1,3 @@
-import glob from 'glob';
-import path from 'path';
 import less from 'rollup-plugin-less';
 import svgr from '@svgr/rollup';
 
@@ -17,14 +15,6 @@ const postcssConfig = require('./postcss.config');
 module.exports = {
   input: {
     bundle: 'lib/index.js',
-    ...glob.sync('lib/utils/*.js').reduce((previousValue, currentValue) => {
-      const file = currentValue.replace('lib/', '');
-
-      // eslint-disable-next-line no-param-reassign
-      previousValue[file.replace(path.extname(file.replace('utils/', '')), '')] = currentValue;
-
-      return previousValue;
-    }, {}),
   },
   output: [
     {
@@ -92,6 +82,7 @@ module.exports = {
           dest: 'dist/',
           rename: (_, __, fullPath) => `${fullPath.substr(fullPath.indexOf('components/'))}`,
         },
+        { src: 'lib/utils/*.js', dest: 'dist/utils' },
       ],
     }),
   ],
